47a84219cb9d75f19d5aad4e9c0675a70c76230f
Elton Lopes Pereira <87839129+Elton-Ti@users.noreply.github.com>
2023-12-18 20:01:07 -0300

    placas

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
 import Modal from "react-modal";
-import YouTubeVideo from './YouTubeVideo';
+import YouTubeVideo from "./YouTubeVideo";
 import CustomImageCarousel from "./CustomImageCarousel";
 import "./CustomImageCarousel.module.css"; // Importe os estilos CSS diretamente
 
 const PortfolioItem = ({ project, onClick }) => (
   <div className="columns portfolio-item">
-    <div className="item-wrap">
+    <div className="item-wrap" style={{ border: "1px #ddd", borderRadius: "8px", padding: "1px" }}>
       <Zmage
         alt={project.title}
         src={`images/portfolio/${project.image}`}
         onClick={() => onClick(project)}
         className="portfolio-image"
+        style={{
+          border: "1px solid #ddd", // Adiciona uma borda simples
+          borderRadius: "8px", // Adiciona bordas arredondadas, ajuste conforme necessário
+        }}
       />
-      <div className="portfolio-title">{project.title}</div>
+      <div className="portfolio-title" style={{ textAlign: "center" }}>
+        {project.title}
+      </div>
     </div>
   </div>
 );
@@ -28,12 +34,10 @@ const ProjectModal = ({ selectedProject, closeModal }) => (
     <br />
     <br />
 
-
     <CustomImageCarousel images={selectedProject.images} />
     <br />
     <br />
     <br />
-
 
     <div className="modal-content">
       <span className="modal-text">{selectedProject.text}</span>
@@ -41,15 +45,33 @@ const ProjectModal = ({ selectedProject, closeModal }) => (
       <br />
       <br />
 
-      {selectedProject.video && <YouTubeVideo videoId={selectedProject.video} />}
-{selectedProject.video1 && <YouTubeVideo videoId={selectedProject.video1} />}
-{selectedProject.video2 && <YouTubeVideo videoId={selectedProject.video2} />}
-{selectedProject.video3 && <YouTubeVideo videoId={selectedProject.video3} />}
-{selectedProject.video4 && <YouTubeVideo videoId={selectedProject.video4} />}
-{selectedProject.video5 && <YouTubeVideo videoId={selectedProject.video5} />}
-{selectedProject.video6 && <YouTubeVideo videoId={selectedProject.video6} />}
-{selectedProject.video7 && <YouTubeVideo videoId={selectedProject.video7} />}
-{selectedProject.video8 && <YouTubeVideo videoId={selectedProject.video8} />}
+      {selectedProject.video && (
+        <YouTubeVideo videoId={selectedProject.video} />
+      )}
+      {selectedProject.video1 && (
+        <YouTubeVideo videoId={selectedProject.video1} />
+      )}
+      {selectedProject.video2 && (
+        <YouTubeVideo videoId={selectedProject.video2} />
+      )}
+      {selectedProject.video3 && (
+        <YouTubeVideo videoId={selectedProject.video3} />
+      )}
+      {selectedProject.video4 && (
+        <YouTubeVideo videoId={selectedProject.video4} />
+      )}
+      {selectedProject.video5 && (
+        <YouTubeVideo videoId={selectedProject.video5} />
+      )}
+      {selectedProject.video6 && (
+        <YouTubeVideo videoId={selectedProject.video6} />
+      )}
+      {selectedProject.video7 && (
+        <YouTubeVideo videoId={selectedProject.video7} />
+      )}
+      {selectedProject.video8 && (
+        <YouTubeVideo videoId={selectedProject.video8} />
+      )}
     </div>
 
     <p></p>
@@ -82,10 +104,18 @@ const Portfolio = ({ data }) => {
   return (
     <section id="portfolio">
       <Fade left duration={1000} distance="40px">
-        <div className="row">
+        <div className="row" style={{ borderRadius: "20%" }}>
           <div className="twelve columns collapsed">
             <h1>Projetos dos Alunos</h1>
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+              style={{
+                border: "1px solid #ddd",
+                borderRadius: "8px",
+                padding: "20px",
+              }}
+            >
               {projects}
             </div>
           </div>
@@ -96,30 +126,40 @@ const Portfolio = ({ data }) => {
       <Modal
         isOpen={modalOpen}
         onRequestClose={closeModal}
-        swipeDirection='down'
+        swipeDirection="down"
         contentLabel="Project Description"
         style={{
           overlay: {
             backgroundColor: "rgba(0, 0, 0, 0.75)",
           },
           content: {
-            top: "10%",
-            left: "2%",
-            transform: "translate(0%, 0%)",
+            top: "50%", // Posição vertical no meio
+            left: "50%", // Posição horizontal no meio
+            transform: "translate(-50%, -50%)", // Centraliza o modal
             maxWidth: "1300px",
             maxHeight: "90%",
-            border: "none",
+            border: "1px solid #ccc",
             borderRadius: "8px",
             padding: "20px",
             overflow: "auto",
             scrollbarWidth: "thin",
+            position: "relative",
           },
         }}
       >
-        <button onClick={closeModal} className="close-button">
+        <button
+          onClick={closeModal}
+          className="close-button"
+          style={{ position: "absolute", top: "10px", right: "10px" }}
+        >
           &times;
         </button>
-        {selectedProject && <ProjectModal selectedProject={selectedProject} closeModal={closeModal} />}
+        {selectedProject && (
+          <ProjectModal
+            selectedProject={selectedProject}
+            closeModal={closeModal}
+          />
+        )}
       </Modal>
     </section>
   );
